de764b7a69654c80c6f0328852ea256e781f2f6c
Nightmayr <umayr23@gmail.com>
2019-01-23 15:10:35 +0000

    Added methods to Comment repo

--- src/main/java/com/qa/repository/CommentRepository.java
+++ src/main/java/com/qa/repository/CommentRepository.java
@@ -1,5 +1,9 @@
 package com.qa.repository;
 
 public interface CommentRepository {
-
+	
+	String getAllComments();
+	String addComment(String comment);
+	String deleteComment(Long id);
+	String updateComment(Long id, String comment);
 }
