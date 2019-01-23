f769da5046409c9c14098cdcb94d76ff9d397239
Nightmayr <umayr23@gmail.com>
2019-01-23 15:05:51 +0000

    Added methods to User repo

--- src/main/java/com/qa/repository/UserRepository.java
+++ src/main/java/com/qa/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.qa.repository;
 
 public interface UserRepository {
-
+	
+	String getAllUsers();
+	String addUser(String user);
+	String deleteUser(Long id);
+	String updateUser(Long id, String user);
 }
