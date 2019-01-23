1318995eb939af223906af55b4bb486839ff36a4
Nightmayr <umayr23@gmail.com>
2019-01-23 16:41:17 +0000

    Added methods to service interfaces

--- src/main/java/com/qa/business/service/RestaurantService.java
+++ src/main/java/com/qa/business/service/RestaurantService.java
@@ -1,5 +1,8 @@
 package com.qa.business.service;
 
 public interface RestaurantService {
-
+	String getAllRestaurants();
+	String addRestaurant(String restaurant);
+	String deleteRestaurant(Long id);
+	String updateRestaurant(Long id, String restaurant);
 }
--- src/main/java/com/qa/business/service/UserService.java
+++ src/main/java/com/qa/business/service/UserService.java
@@ -1,5 +1,8 @@
 package com.qa.business.service;
 
 public interface UserService {
-
+	String getAllUsers();
+	String addUser(String user);
+	String deleteUser(Long id);
+	String updateUser(Long id, String user);
 }
