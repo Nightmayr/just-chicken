250fe8cfd689920707f7c6d48b59cabb6ad12a7d
Nightmayr <umayr23@gmail.com>
2019-01-23 15:04:58 +0000

    Added methods to Restaurant repo

--- src/main/java/com/qa/repository/RestaurantRepository.java
+++ src/main/java/com/qa/repository/RestaurantRepository.java
@@ -1,5 +1,10 @@
 package com.qa.repository;
 
 public interface RestaurantRepository {
+	
+	String getAllRestaurants();
+	String addRestaurant(String restaurant);
+	String deleteRestaurant(Long id);
+	String updateRestaurant(Long id, String restaurant);
 
 }
