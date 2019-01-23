ea793e133827872f6323ad6cb8486fba96c76bbb
Nightmayr <umayr23@gmail.com>
2019-01-23 16:52:04 +0000

    Added methods to service implementations

--- src/main/java/com/qa/business/service/CommentServiceImplementation.java
+++ src/main/java/com/qa/business/service/CommentServiceImplementation.java
@@ -1,5 +1,34 @@
 package com.qa.business.service;
 
-public class CommentServiceImplementation {
+import javax.inject.Inject;
 
+import com.qa.persistence.domain.Comment;
+import com.qa.persistence.repository.CommentRepository;
+import com.qa.util.JSONUtil;
+
+public class CommentServiceImplementation implements CommentService {
+
+	@Inject
+	private CommentRepository repo;
+	@Inject
+	private JSONUtil util;
+	
+	
+	public String getAllComments() {
+		return repo.getAllComments();
+	}
+
+	public String addComment(String comment) {
+		Comment checkComment = util.getObjectForJSON(comment, Comment.class);
+		return repo.addComment(comment);
+	}
+
+	public String deleteComment(Long id) {
+		return repo.deleteComment(id);
+	}
+
+	public String updateComment(Long id, String comment) {
+		return repo.updateComment(id, comment);
+	}
+	
 }
--- src/main/java/com/qa/business/service/UserServiceImplementation.java
+++ src/main/java/com/qa/business/service/UserServiceImplementation.java
@@ -1,5 +1,34 @@
 package com.qa.business.service;
 
-public class UserServiceImplementation {
+import javax.inject.Inject;
 
+import com.qa.persistence.domain.User;
+import com.qa.persistence.repository.UserRepository;
+import com.qa.util.JSONUtil;
+
+public class UserServiceImplementation implements UserService {
+
+	@Inject
+	private UserRepository repo;
+	@Inject
+	private JSONUtil util;
+	
+	
+	public String getAllUsers() {
+		return repo.getAllUsers();
+	}
+
+	public String addUser(String user) {
+		User checkUser = util.getObjectForJSON(user, User.class);
+		return repo.addUser(user);
+	}
+
+	public String deleteUser(Long id) {
+		return repo.deleteUser(id);
+	}
+
+	public String updateUser(Long id, String user) {
+		return repo.updateUser(id, user);
+	}
+	
 }
